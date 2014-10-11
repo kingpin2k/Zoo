2f9ab93578c687a1d7564862952fb543a483a51c
kingpin2k <kingpin2k@gmail.com>
2014-10-11 12:21:21 -0600

    dd I should show an example of a controller

---
+++ Zoo/Scripts/Zoo/Controllers/animal.js
@@ -0,0 +1,5 @@
+﻿Zoo.AnimalController = Em.ObjectController.extend({
+    daysOld: function () {
+        return moment(this.get('date_of_birth')).fromNow();
+    }.property('date_of_birth')
+});
--- Zoo/Scripts/Zoo/Models/animal.js
+++ Zoo/Scripts/Zoo/Models/animal.js
@@ -2,8 +2,5 @@
     name: DS.attr(),
     date_of_birth: DS.attr(),
     image: DS.attr(),
-    animal_type: DS.attr(),
-    daysOld: function () {
-        return moment(this.get('date_of_birth')).fromNow();//('L LT');
-    }.property('date_of_birth')
+    animal_type: DS.attr()
 });
